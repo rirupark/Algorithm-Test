79246447c5bc85762cb4333aa0bfec3152e39236
Minju <mlnjv016@gmail.com>
2022-06-24 17:12:48 +0900

    Lv1. sumOfInfs

---
+++ Algorithm Test/Level 1/Lv1. sumOfInts.swift
@@ -0,0 +1,18 @@
+//
+//  Lv1. sumOfInts.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2022/06/24.
+//
+
+import Foundation
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+  var absolutes = absolutes
+  
+  for i in 0..<signs.count {
+    if signs[i] == false { absolutes[i] = -absolutes[i] }
+  }
+  
+  return absolutes.reduce(0) { $0 + $1 }
+}
