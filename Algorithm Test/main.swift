59cc6df1f712adc26d2f5608798388c5252d7906
Minju <mlnjv016@gmail.com>
2022-07-04 22:07:33 +0900

    main init

--- Algorithm Test/main.swift
+++ Algorithm Test/main.swift
@@ -1,18 +1,5 @@
 //  Algorithm Test main 실행파일
 //  Created by 박민주 on 2022/06/21
 
-import Foundation
 
-func solution(_ d:[Int], _ budget:Int) -> Int {
-    var sum = 0
-    var result = 0
-    
-    for element in d.sorted() {
-        sum += element
-        result += 1
-        if sum > budget { result -= 1; break }
-    }
-    
-    return result
-}
 
