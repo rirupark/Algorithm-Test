45cbb5e419054faff4fbba5821d0d0d18dd681de
Minju <mlnjv016@gmail.com>
2022-07-06 19:06:41 +0900

    Lv1. sumOfDivisor solution

---
+++ Algorithm Test/Level 1/Lv1. sumOfDivisor.swift
@@ -0,0 +1,25 @@
+//
+//  Lv1. sumOfDivisor.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2022/07/06.
+//
+
+import Foundation
+
+func solutionDivisor(_ left:Int, _ right:Int) -> Int {
+    var result = 0
+    
+    for num in left...right {
+        var divisors = 0
+        
+        // 각 숫자들의 약수 개수 구하기
+        for i in 1...num { if num % i == 0 { divisors += 1 }}
+        
+        // 각 숫자들의 약수 개수의 짝홀 경우 처리
+        if divisors % 2 == 0 { result += num }
+        else { result -= num }
+    }
+    
+    return result
+}
--- Algorithm Test/main.swift
+++ Algorithm Test/main.swift
@@ -1,5 +1,4 @@
 //  Algorithm Test main 실행파일
 //  Created by 박민주 on 2022/06/21
 
-import Foundation
 
