618ed045b074c1878162907b6feb679e95435762
Minju <mlnjv016@gmail.com>
2022-07-07 14:46:33 +0900

    Lv1. toSortString make file

---
+++ Algorithm Test/Level 1/Lv1. toSortString.swift
@@ -0,0 +1,8 @@
+//
+//  Lv1. toSortString.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2022/07/07.
+//
+
+import Foundation
--- Algorithm Test/main.swift
+++ Algorithm Test/main.swift
@@ -2,5 +2,10 @@
 //  Created by 박민주 on 2022/06/21
 
 
+func solutionSortString(_ strings:[String], _ n:Int) -> [String] {
+    return []
+}
 
+
+print(solutionSortString(["sun", "bed", "car"], 1))
 
