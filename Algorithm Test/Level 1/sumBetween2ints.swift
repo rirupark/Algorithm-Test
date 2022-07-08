b30eafdb1e98376ae06b71d62ec3d88304ce383c
Minju <mlnjv016@gmail.com>
2022-07-08 16:11:09 +0900

    Lv1. sumBetween2ints solution

--- Algorithm Test/Level 1/sumBetween2ints.swift
+++ Algorithm Test/Level 1/sumBetween2ints.swift
@@ -5,4 +5,8 @@
 //  Created by 박민주 on 2022/07/08.
 //
 
-import Foundation
+func solutionBetweenInts(_ a:Int, _ b:Int) -> Int64 {
+    var result = 0
+    for num in (a > b ? b...a : a...b) { result += num }
+    return Int64(result)
+}
