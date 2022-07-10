dcac51f0929453e515a71302adbb3ba3b566445f
Minju <mlnjv016@gmail.com>
2022-07-10 16:50:15 +0900

    Lv1. Mr.Kim solution

---
+++ Algorithm Test/Level 1/Lv1. Mr.Kim.swift
@@ -0,0 +1,10 @@
+//
+//  Lv1. Mr.Kim.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2022/07/10.
+//
+
+func solutionKim(_ seoul:[String]) -> String {
+    return "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"
+}
---
+++ Algorithm Test/Level 1/Lv1. filpNum.swift
@@ -0,0 +1,8 @@
+//
+//  Lv1. filpNum.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2022/07/09.
+//
+
+import Foundation
