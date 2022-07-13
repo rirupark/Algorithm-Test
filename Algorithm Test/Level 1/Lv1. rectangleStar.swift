12c3e092d7cd3a3f82c3113ed6e4ea18718a3dd7
Minju <mlnjv016@gmail.com>
2022-07-13 12:27:08 +0900

    Lv1. rectangleStar 주석처리

--- Algorithm Test/Level 1/Lv1. rectangleStar.swift
+++ Algorithm Test/Level 1/Lv1. rectangleStar.swift
@@ -5,8 +5,8 @@
 //  Created by 박민주 on 2022/07/13.
 //
 
-import Foundation
-
+//import Foundation
+//
 //let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
 //let (a, b) = (n[0], n[1])
 //for _ in 1...b {
