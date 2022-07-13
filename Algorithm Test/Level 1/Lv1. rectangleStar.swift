a2b9ba29a9f8fa481a20bccac706460bfd1a57ad
Minju <mlnjv016@gmail.com>
2022-07-13 12:02:17 +0900

    Lv1. rectangleStar solution

---
+++ Algorithm Test/Level 1/Lv1. rectangleStar.swift
@@ -0,0 +1,15 @@
+//
+//  Lv1. rectangleStar.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2022/07/13.
+//
+
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+for _ in 1...b {
+    for _ in 1...a { print("*", terminator: "") }
+    print(terminator: "\n")
+}
