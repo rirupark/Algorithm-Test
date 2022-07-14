68f4b34d49b72532aa013fcf22907e69b8fb61a6
Minju <mlnjv016@gmail.com>
2022-07-14 14:17:47 +0900

    Lv1. Odd&Even solution

---
+++ Algorithm Test/Level 1/Lv1. Odd&Even.swift
@@ -0,0 +1,10 @@
+//
+//  Lv1. Odd&Even.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2022/07/14.
+//
+
+func solutionOddEven(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
