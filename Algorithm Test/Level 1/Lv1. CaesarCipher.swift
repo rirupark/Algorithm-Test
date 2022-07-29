82650c129842ddbe9762e25a5aff62a83011262a
Minju <mlnjv016@gmail.com>
2022-07-29 21:13:30 +0900

    Lv1. CaesarCipher solution

--- Algorithm Test/Level 1/Lv1. CaesarCipher.swift
+++ Algorithm Test/Level 1/Lv1. CaesarCipher.swift
@@ -5,4 +5,14 @@
 //  Created by 박민주 on 2022/07/20.
 //
 
-import Foundation
+func solutionCaesar(_ s:String, _ n:Int) -> String {
+    var ans = ""
+    for char in s {
+        if char.isLetter {
+            if 65 <= char.asciiValue! && char.asciiValue! <= 90 { ans += String(UnicodeScalar((char.asciiValue! + UInt8(n) - 65) % 26 + 65 )) }
+            if 97 <= char.asciiValue! && char.asciiValue! <= 122 { ans += String(UnicodeScalar((char.asciiValue! + UInt8(n) - 97) % 26 + 97 )) }
+        }
+        else { ans += " " }
+    }
+    return ans
+}
