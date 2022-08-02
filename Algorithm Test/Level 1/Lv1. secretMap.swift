68c46f63c9aead73b9a945918029bbdc66a146ba
Minju <mlnjv016@gmail.com>
2022-08-02 19:51:06 +0900

    Lv1. secretMap solution

--- Algorithm Test/Level 1/Lv1. secretMap.swift
+++ Algorithm Test/Level 1/Lv1. secretMap.swift
@@ -5,4 +5,16 @@
 //  Created by 박민주 on 2022/08/01.
 //
 
-import Foundation
+func solutionSecretMap(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    var answer: [String] = []
+    
+    for i in 0..<n {
+        var str = String(arr1[i] | arr2[i], radix: 2)
+        var result = ""
+        while str.count < n { str.insert("0", at: str.index(str.startIndex, offsetBy: 0)) }
+        for char in str { result += char == "0" ? " " : "#" }
+        answer.append(result)
+    }
+    return answer
+}
+
