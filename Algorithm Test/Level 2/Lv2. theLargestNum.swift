fb566dc98613522736137ca6ef94f2a1a98c3031
Minju <mlnjv016@gmail.com>
2022-08-03 22:17:32 +0900

    Lv2. theLargestNum make file & make func in main file

---
+++ Algorithm Test/Level 2/Lv2. theLargestNum.swift
@@ -0,0 +1,8 @@
+//
+//  Lv2. theLargestNum.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2022/08/03.
+//
+
+import Foundation
--- Algorithm Test/main.swift
+++ Algorithm Test/main.swift
@@ -2,3 +2,10 @@
 //  Created by 박민주 on 2022/06/21
 
 
+import Foundation
+
+func solutionLargestNum(_ numbers:[Int]) -> String {
+    return ""
+}
+
+print(solutionLargestNum([6, 10, 2]))
