c1a864c19671c58ebcf677668beeb1d6a86c2985
Minju <mlnjv016@gmail.com>
2022-08-04 22:28:31 +0900

    Lv2. theLargestNum solution

--- Algorithm Test/Level 2/Lv2. theLargestNum.swift
+++ Algorithm Test/Level 2/Lv2. theLargestNum.swift
@@ -5,4 +5,9 @@
 //  Created by 박민주 on 2022/08/03.
 //
 
-import Foundation
+func solutionLargestNum(_ numbers:[Int]) -> String {
+    let nums = numbers.map{String($0)}.sorted{ $0 + $1 > $1 + $0 }
+    let answer = nums.joined()
+    if let num = Int(answer) { return String(num) }
+    return answer
+}
--- Algorithm Test/main.swift
+++ Algorithm Test/main.swift
@@ -2,10 +2,4 @@
 //  Created by 박민주 on 2022/06/21
 
 
-import Foundation
 
-func solutionLargestNum(_ numbers:[Int]) -> String {
-    return ""
-}
-
-print(solutionLargestNum([6, 10, 2]))
