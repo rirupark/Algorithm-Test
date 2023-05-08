30a9ab3f2757fa0b6bbf9caf7ab8e19ce9b145c9
Minju Park <mlnjv016@gmail.com>
2023-05-09 00:40:48 +0900

    Baekjoon solution

---
+++ Algorithm Test/Baekjoon/15894. 수학은 체육과목 입니다.swift
@@ -0,0 +1,11 @@
+//
+//  15894. 수학은 체육과목 입니다.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/05/08.
+//
+
+func B15894() {
+    let n = Int(readLine()!)!
+    print(n * 4)
+}
---
+++ Algorithm Test/Baekjoon/24262. 알고리즘의 수행 시간 1.swift
@@ -0,0 +1,11 @@
+//
+//  24262. 알고리즘의 수행 시간 1.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/05/09.
+//
+
+func B24262() {
+    print(1)
+    print(0)
+}
