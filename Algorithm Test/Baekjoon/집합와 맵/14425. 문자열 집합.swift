01becaf426aae2ea7082b175118b884d912ba747
Minju Park <mlnjv016@gmail.com>
2023-05-11 23:12:21 +0900

    BOJ 14425 solution

--- Algorithm Test/Baekjoon/집합와 맵/14425. 문자열 집합.swift
+++ Algorithm Test/Baekjoon/집합와 맵/14425. 문자열 집합.swift
@@ -4,7 +4,15 @@
 //
 //  Created by 박민주 on 2023/05/11.
 //
+// 자료구조, 문자열, 해시를 사용한 집합과 맵, 트리를 사용한 집합과 맵
 
 func B14425() {
-    
+    let nm = readLine()!.split(separator: " ").map{Int($0)!}
+    var dic = [String:Int]()
+    var answer = 0
+    for _ in 0..<nm[0] { dic[readLine()!] = 1 }
+    for _ in 0..<nm[1] {
+        if dic[readLine()!] != nil { answer += 1 }
+    }
+    print(answer)
 }
