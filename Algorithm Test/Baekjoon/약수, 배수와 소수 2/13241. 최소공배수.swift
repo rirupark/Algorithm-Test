3c096f95e4022b54fb34ff22c01a4659b655e936
Minju Park <mlnjv016@gmail.com>
2023-05-13 00:04:05 +0900

    BOJ 13241 solution

--- Algorithm Test/Baekjoon/약수, 배수와 소수 2/13241. 최소공배수.swift
+++ Algorithm Test/Baekjoon/약수, 배수와 소수 2/13241. 최소공배수.swift
@@ -4,7 +4,15 @@
 //
 //  Created by 박민주 on 2023/05/12.
 //
+// 수학, 정수론, 유클리드 호제법
 
 func B13241() {
-    
+    let ab = readLine()!.split(separator: " ").map{Int($0)!}.sorted(by: >)
+    var a = ab[0], b = ab[1]
+    while b > 0 {
+        let tmp = a
+        a = b
+        b = tmp % b
+    }
+    print(ab[0] / a * ab[1])
 }
