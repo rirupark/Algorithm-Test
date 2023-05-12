1e40130058bda97e49c65326c02a34e2e6e9f4e7
Minju Park <mlnjv016@gmail.com>
2023-05-12 23:18:37 +0900

    BOJ 11478 solution

--- Algorithm Test/Baekjoon/집합와 맵/11478. 서로 다른 부분 문자열의 개수.swift
+++ Algorithm Test/Baekjoon/집합와 맵/11478. 서로 다른 부분 문자열의 개수.swift
@@ -4,7 +4,18 @@
 //
 //  Created by 박민주 on 2023/05/11.
 //
+// 자료 구조, 문자열, 해시를 사용한 집합과 맵, 트리를 사용한 집합과 맵
 
 func B11478() {
-    
+    var s = readLine()!
+    var sSet = Set<String>()
+    for _ in s {
+        var str = ""
+        for char in s {
+            str += String(char)
+            sSet.insert(str)
+        }
+        s.removeFirst()
+    }
+    print(sSet.count)
 }
