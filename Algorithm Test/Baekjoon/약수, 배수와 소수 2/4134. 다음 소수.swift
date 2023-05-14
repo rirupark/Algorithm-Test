feb8dc0e568f35a05a4f2b5f3d1039fc0ac7e152
Minju Park <mlnjv016@gmail.com>
2023-05-14 17:49:44 +0900

    BOJ 4134 solution

--- Algorithm Test/Baekjoon/약수, 배수와 소수 2/4134. 다음 소수.swift
+++ Algorithm Test/Baekjoon/약수, 배수와 소수 2/4134. 다음 소수.swift
@@ -4,7 +4,38 @@
 //
 //  Created by 박민주 on 2023/05/12.
 //
+// 수학, 브루트포스 알고리즘, 정수론, 소수 판정
 
+import Foundation
+
+// 79508KB, 128ms
 func B4134() {
-    
+    let t = Int(readLine()!)!
+    for _ in 0..<t {
+        var n = Int(readLine()!)!
+        while !isPrime(n) {
+            n += 1
+        }
+        print(n)
+    }
+
+    func isPrime(_ num: Int) -> Bool {
+        if num < 2 { return false }
+        for i in 2..<Int(sqrt(Double(num))) + 1 {
+            if num % i == 0 { return false }
+        }
+        return true
+    }
 }
+
+// MARK: isPrime 메모리 개선
+// 69100KB, 148ms
+//func isPrime(_ num: Int) -> Bool {
+//    if num < 2 { return false }
+//    var index = 2
+//    while index * index <= num {
+//        if num % index == 0 { return false }
+//        index += 1
+//    }
+//    return true
+//}
--- Algorithm Test/main.swift
+++ Algorithm Test/main.swift
@@ -2,5 +2,6 @@
 //  Algorithm Test main 실행파일
 //  Created by 박민주 on 2022/06/21
 
+import Foundation
 
 
