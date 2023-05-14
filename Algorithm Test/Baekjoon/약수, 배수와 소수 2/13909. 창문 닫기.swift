7ed0bee01e0dd3df48e62557464ff6d6d1875c80
Minju Park <mlnjv016@gmail.com>
2023-05-15 00:39:07 +0900

    BOJ 13909 solution

--- Algorithm Test/Baekjoon/약수, 배수와 소수 2/13909. 창문 닫기.swift
+++ Algorithm Test/Baekjoon/약수, 배수와 소수 2/13909. 창문 닫기.swift
@@ -4,7 +4,12 @@
 //
 //  Created by 박민주 on 2023/05/12.
 //
+// 수학, 정수론
 
+// 각 숫자의 약수의 개수가 홀수여야 함. => 제곱수인 것
 func B13909() {
-    
+    let n = Int(readLine()!)!
+    var t = 1, count = 0
+    while t * t <= n { count += 1; t += 1 }
+    print(count)
 }
--- Algorithm Test/main.swift
+++ Algorithm Test/main.swift
@@ -1,3 +1,4 @@
 //
 //  Algorithm Test main 실행파일
 //  Created by 박민주 on 2022/06/21
+
