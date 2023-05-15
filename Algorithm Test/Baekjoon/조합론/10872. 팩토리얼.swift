2151dab43341b6acb40034f707b26f8ff0e3de0c
Minju Park <mlnjv016@gmail.com>
2023-05-15 19:00:37 +0900

    BOJ 10872 solution

--- Algorithm Test/Baekjoon/조합론/10872. 팩토리얼.swift
+++ Algorithm Test/Baekjoon/조합론/10872. 팩토리얼.swift
@@ -4,7 +4,13 @@
 //
 //  Created by 박민주 on 2023/05/15.
 //
+// 수학, 구현, 조합론
 
 func B10872() {
-    
+    let n = Int(readLine()!)!
+    var answer = 1
+    if n > 0 {
+        for num in 1...n { answer *= num }
+    }
+    print(answer)
 }
