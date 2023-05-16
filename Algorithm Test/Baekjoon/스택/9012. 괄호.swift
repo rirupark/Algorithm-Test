8ddea7a214c33ee3081dc36267a176ae248fe76f
Minju Park <mlnjv016@gmail.com>
2023-05-16 21:11:05 +0900

    BOJ 9012 solution

--- Algorithm Test/Baekjoon/스택/9012. 괄호.swift
+++ Algorithm Test/Baekjoon/스택/9012. 괄호.swift
@@ -4,5 +4,28 @@
 //
 //  Created by 박민주 on 2023/05/16.
 //
+// 자료 구조, 문자열, 스택
 
-import Foundation
+func B9012() {
+    var array = [Character]()
+    let t = Int(readLine()!)!
+    for _ in 0..<t {
+      let input = readLine()!
+      var count = 0
+      for i in input {
+        if i == "(" {
+          array.append(i)
+          count += 1
+        }
+        else {
+          if array.count != 0 {
+            array.removeLast()
+            count -= 1
+          }
+          else { count += 1 }
+        }
+      }
+      print(count == 0 ? "YES" : "NO")
+      array.removeAll()
+    }
+}
