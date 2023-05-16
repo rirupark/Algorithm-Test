bfe053fb9c867dc7d9ac1f11348aa88d753120d2
Minju Park <mlnjv016@gmail.com>
2023-05-16 21:12:43 +0900

    BOJ 1874 solution

--- Algorithm Test/Baekjoon/스택/1874. 스택 수열.swift
+++ Algorithm Test/Baekjoon/스택/1874. 스택 수열.swift
@@ -4,5 +4,28 @@
 //
 //  Created by 박민주 on 2023/05/16.
 //
-
 import Foundation
+
+func B1874() {
+    var stack : [Int] = []
+    var result : [String] = []
+    let n = Int(readLine()!)!
+    var data = 1, count = 0
+
+    for _ in 0..<n {
+      let num = Int(readLine()!)!
+      
+      while data <= num  {
+        stack.append(data)
+        result.append("+")
+        data += 1
+      }
+
+      if stack.last! == num {
+        stack.removeLast()
+        result.append("-")
+      }
+      else { count = 1 }
+    }
+    print(count != 0 ? "NO" : result.joined(separator:"\n"))
+}
