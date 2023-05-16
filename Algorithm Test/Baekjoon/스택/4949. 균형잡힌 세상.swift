e60e432924024e744f529ca21d455412d642ee87
Minju Park <mlnjv016@gmail.com>
2023-05-16 22:01:06 +0900

    BOJ 4949 solution

--- Algorithm Test/Baekjoon/스택/4949. 균형잡힌 세상.swift
+++ Algorithm Test/Baekjoon/스택/4949. 균형잡힌 세상.swift
@@ -4,5 +4,24 @@
 //
 //  Created by 박민주 on 2023/05/16.
 //
+// 자료 구조, 문자열, 스택
 
-import Foundation
+func B4949() {
+    while true {
+        var stack = [Character]()
+        let input = readLine()!
+        if input == "." { break }
+        for char in input {
+            if char == "(" || char == "[" { stack.append(char) }
+            if char == ")" {
+                if stack.last == "(" { stack.removeLast() }
+                else { stack.append(")") }
+            }
+            if char == "]" {
+                if stack.last == "[" { stack.removeLast() }
+                else { stack.append("]") }
+            }
+        }
+        print(stack.isEmpty ? "yes" : "no")
+    }
+}
