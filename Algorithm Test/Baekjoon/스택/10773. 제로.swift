bb9e0385f18e677c75088e3d73d813f5568f3ec6
Minju Park <mlnjv016@gmail.com>
2023-05-16 21:09:44 +0900

    BOJ 10773 solution

--- Algorithm Test/Baekjoon/스택/10773. 제로.swift
+++ Algorithm Test/Baekjoon/스택/10773. 제로.swift
@@ -4,5 +4,15 @@
 //
 //  Created by 박민주 on 2023/05/16.
 //
+// 구현, 자료 구조, 스택
 
-import Foundation
+func B10773() {
+    let k = Int(readLine()!)!
+    var arr = [Int]()
+    for _ in 0..<k {
+        let num = Int(readLine()!)!
+        if num == 0 { arr.removeLast() }
+        else { arr.append(num) }
+    }
+    print(arr.reduce(0, +))
+}
