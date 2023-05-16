a8b873a8c1e46bc18950d808ce0eccfd68c0896e
Minju Park <mlnjv016@gmail.com>
2023-05-16 17:53:47 +0900

    BOJ 심화 2 파일 생성

---
+++ Algorithm Test/Baekjoon/심화 2/1037. 약수.swift
@@ -0,0 +1,10 @@
+//
+//  1037. 약수.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/05/16.
+//
+
+func B1037() {
+    
+}
---
+++ Algorithm Test/Baekjoon/심화 2/2108. 통계학.swift
@@ -0,0 +1,10 @@
+//
+//  2108. 통계학.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/05/16.
+//
+
+func B2108() {
+    
+}
