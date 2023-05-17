671a73d84b4e1b3c2b18a428e9aa1329be7327c4
Minju Park <mlnjv016@gmail.com>
2023-05-18 00:07:56 +0900

    BOJ 10866 solution

--- Algorithm Test/Baekjoon/큐, 덱/10866. 덱.swift
+++ Algorithm Test/Baekjoon/큐, 덱/10866. 덱.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+func B10866() {
+    var deque : [Int] = []
+    let n = Int(readLine()!)!
+    
+    for _ in 0..<n {
+        let input = readLine()!.split(separator:" ")
+        let cmd = String(input[0])
+        
+        if cmd == "push_front" {
+            deque.insert(Int(input[1])!, at: 0)
+        }
+        
+        else if cmd == "push_back" {
+            deque.append(Int(input[1])!)
+        }
+        
+        else if cmd == "pop_front" {
+            print(deque.isEmpty ? -1 : deque.removeFirst())
+        }
+        
+        else if cmd == "pop_back" {
+            if deque.isEmpty {
+                print(-1)
+            }
+            else {
+                print(deque.last!)
+                deque.removeLast()
+            }
+        }
+        
+        else if cmd == "size" {
+            print(deque.count)
+        }
+        
+        else if cmd == "empty" {
+            print(deque.isEmpty ? 1 : 0)
+        }
+        
+        else if cmd == "front" {
+            print(deque.isEmpty ? -1 : deque[0])
+        }
+        
+        else if cmd == "back" {
+            print(deque.isEmpty ? -1 : deque.last!)
+        }
+    }
+}
