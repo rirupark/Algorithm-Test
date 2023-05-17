fc3b9a61c345c02bd52f27a0a372c509c7487ef9
Minju Park <mlnjv016@gmail.com>
2023-05-17 18:10:54 +0900

    BOJ 2164 solution

--- Algorithm Test/Baekjoon/큐, 덱/2164. 카드2.swift
+++ Algorithm Test/Baekjoon/큐, 덱/2164. 카드2.swift
@@ -4,5 +4,18 @@
 //
 //  Created by 박민주 on 2023/05/16.
 //
+// 자료 구조, 큐
 
-import Foundation
+func B2164() {
+    let n = Int(readLine()!)!
+    var arr = Array(1...n)
+    var front = 0
+    for _ in 0..<arr.count - 1 {
+        arr.append(arr[front + 1])
+        front += 2
+    }
+    print(arr[front])
+}
+
+// MARK: Swift에서 시간 초과를 막기 위한 큐 구현 방법
+// 1. 포인터 사용  2. 더블 스택 큐
