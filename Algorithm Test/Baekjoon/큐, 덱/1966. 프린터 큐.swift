16d3f22aa1427968ade616bcb060476db7b38ae8
Minju Park <mlnjv016@gmail.com>
2023-05-18 00:04:51 +0900

    BOJ 1966 solution

--- Algorithm Test/Baekjoon/큐, 덱/1966. 프린터 큐.swift
+++ Algorithm Test/Baekjoon/큐, 덱/1966. 프린터 큐.swift
@@ -4,5 +4,26 @@
 //
 //  Created by 박민주 on 2023/05/16.
 //
+// 구현, 자료 구조, 시뮬레이션, 큐
 
-import Foundation
+func B1966() {
+    let t = Int(readLine()!)!
+    for _ in 0..<t {
+        let nm = readLine()!.split(separator: " ").map{Int($0)!}
+        var priority = readLine()!.split(separator: " ").map{Int($0)!}
+        var queue = [(Int, Int)]()
+        var order = 0
+        for (i, v) in priority.enumerated() { queue.append((i, v)) }
+        priority.sort()
+        while true {
+            if queue[0].1 == priority.last { // 0번째 요소와 최댓값 비교
+                order += 1 // 우선순위에 따른 순서 증가 (최댓값 등장부터 카운트)
+                if queue[0].0 == nm[1] { print(order); break }
+                else {
+                    queue.removeFirst()
+                    priority.removeLast()
+                }
+            } else { queue.append(queue.removeFirst()) }
+        }
+    }
+}
