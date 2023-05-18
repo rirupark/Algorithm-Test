8e4fbfdc9dd0e91509f82b76e7dedddb16ad7403
Minju Park <mlnjv016@gmail.com>
2023-05-19 00:08:35 +0900

    BOJ 2447 solution

--- Algorithm Test/Baekjoon/재귀/2447. 별 찍기 - 10.swift
+++ Algorithm Test/Baekjoon/재귀/2447. 별 찍기 - 10.swift
@@ -4,5 +4,16 @@
 //
 //  Created by 박민주 on 2023/05/18.
 //
+// 분할 정복, 재귀
 
-import Foundation
+func B2447() {
+    let n = Int(readLine()!)!
+    star(n, ["*"])
+
+    func star(_ n: Int, _ arr: [String]) {
+        if n <= 1 { arr.forEach{ print($0) }; return }
+        let line1 = arr.map{ $0 + $0 + $0 }
+        let line2 = arr.map{ $0 + String(repeating: " ", count: $0.count) + $0 }
+        star(n / 3, line1 + line2 + line1)
+    }
+}
