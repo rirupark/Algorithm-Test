f3e8f13a3572b141a101a9233f099c718411c5b5
Minju Park <mlnjv016@gmail.com>
2023-05-18 19:43:12 +0900

    BOJ 25501 solution

--- Algorithm Test/Baekjoon/재귀/25501. 재귀의 귀재.swift
+++ Algorithm Test/Baekjoon/재귀/25501. 재귀의 귀재.swift
@@ -4,5 +4,22 @@
 //
 //  Created by 박민주 on 2023/05/18.
 //
+// 구현, 문자열, 재귀
 
-import Foundation
+func B25501() {
+    let t = Int(readLine()!)!
+    var count = 0
+    for _ in 0..<t {
+        count = 0
+        print(isPalindrome(readLine()!.map{$0}), count)
+    }
+    func recursion(_ s: [Character], _ l: Int, _ r: Int) -> Int {
+        count += 1
+        if l >= r { return 1 }
+        else if s[l] != s[r] { return 0 }
+        else { return recursion(s, l + 1, r - 1) }
+    }
+    func isPalindrome(_ s: [Character]) -> Int {
+        return recursion(s, 0, s.count - 1)
+    }
+}
