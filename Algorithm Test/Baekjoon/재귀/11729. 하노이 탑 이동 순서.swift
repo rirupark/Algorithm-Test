1110a745bd1ecf9fe1117a5aa1524503dada0fd9
Minju Park <mlnjv016@gmail.com>
2023-05-19 00:56:51 +0900

    BOJ 11729 solution

--- Algorithm Test/Baekjoon/재귀/11729. 하노이 탑 이동 순서.swift
+++ Algorithm Test/Baekjoon/재귀/11729. 하노이 탑 이동 순서.swift
@@ -4,5 +4,21 @@
 //
 //  Created by 박민주 on 2023/05/18.
 //
+// 재귀
 
-import Foundation
+// (n - 1)개의 원판을 a에서 b, b에서 c로 옮긴다.
+func B11729() {
+    let n = Int(readLine()!)!
+    var answer = "", count = 0
+    hanoi(n, 1, 2, 3)
+    print("\(count)\n\(answer)")
+
+    func hanoi(_ n: Int, _ a: Int, _ b: Int, _ c: Int) {
+        if n == 1 { answer += "\(a) \(c)\n"; count += 1 }
+        else {
+            hanoi(n - 1, a, c, b) // a -> b
+            answer += "\(a) \(c)\n"; count += 1
+            hanoi(n - 1, b, a, c) // b -> c
+        }
+    }
+}
