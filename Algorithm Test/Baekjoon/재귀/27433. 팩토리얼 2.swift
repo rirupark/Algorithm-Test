9008f5d0d87c1633ec12d37b88b942a5a2ce8c44
Minju Park <mlnjv016@gmail.com>
2023-05-18 17:44:03 +0900

    BOJ 27433 solution

--- Algorithm Test/Baekjoon/재귀/27433. 팩토리얼 2.swift
+++ Algorithm Test/Baekjoon/재귀/27433. 팩토리얼 2.swift
@@ -4,5 +4,12 @@
 //
 //  Created by 박민주 on 2023/05/18.
 //
+// 수학, 사칙연산
 
-import Foundation
+func B27433() {
+    func factorialN(_ n: Int) -> Int {
+        if n < 1 { return 1 }
+        return n * factorialN(n - 1)
+    }
+    print(factorialN(Int(readLine()!)!))
+}
