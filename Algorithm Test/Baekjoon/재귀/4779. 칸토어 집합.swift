004f0347cbefef50c858b8aecb1c799a01aeb6d2
Minju Park <mlnjv016@gmail.com>
2023-05-18 21:30:40 +0900

    BOJ 4779 solution

--- Algorithm Test/Baekjoon/재귀/4779. 칸토어 집합.swift
+++ Algorithm Test/Baekjoon/재귀/4779. 칸토어 집합.swift
@@ -4,5 +4,17 @@
 //
 //  Created by 박민주 on 2023/05/18.
 //
+// 분할 정복, 재귀
 
 import Foundation
+
+func B4779() {
+    while let input = readLine(), let n = Double(input) {
+        print(cantor(Int(pow(3.0, n))))
+    }
+    func cantor(_ n: Int) -> String {
+        if n <= 1 { return "-" }
+        let line = cantor(n / 3)
+        return line + String(repeating: " ", count: n / 3) + line
+    }
+}
