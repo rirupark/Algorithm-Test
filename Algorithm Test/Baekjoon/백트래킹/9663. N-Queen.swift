0b8afe2838884e4a531cc16dcbf916f6d4d69817
Minju Park <mlnjv016@gmail.com>
2023-05-21 19:43:34 +0900

    BOJ 9663 주석 추가

--- Algorithm Test/Baekjoon/백트래킹/9663. N-Queen.swift
+++ Algorithm Test/Baekjoon/백트래킹/9663. N-Queen.swift
@@ -7,9 +7,9 @@
 
 func B9663() {
     let n = Int(readLine()!)!
-    var column = Array(repeating: false, count: n)
-    var diagonal1 = Array(repeating: false, count: 2 * n - 1)
-    var diagonal2 = Array(repeating: false, count: 2 * n - 1)
+    var column = Array(repeating: false, count: n) // | 열 임시 배열
+    var diagonal1 = Array(repeating: false, count: 2 * n - 1) // / 대각선 임시 배열
+    var diagonal2 = Array(repeating: false, count: 2 * n - 1) // \ 대각선 임시 배열
     var count = 0
 
     nQueen(0)
@@ -26,9 +26,10 @@ func B9663() {
     func nQueen(_ row: Int) { // 현재 행에 대해 각 열을 순회
         if row == n { count += 1; return }
         for col in 0..<n {
-            if check(row, col) {
+            if check(row, col) { // 백트래킹
                 column[col] = true; diagonal1[row + col] = true; diagonal2[row - col + n - 1] = true
                 nQueen(row + 1)
+                // 모든 경우 탐색을 위해(다음 열 탐색을 위해) 다시 false로 되돌리기
                 column[col] = false; diagonal1[row + col] = false; diagonal2[row - col + n - 1] = false
             }
         }
