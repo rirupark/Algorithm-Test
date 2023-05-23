5bb9f6f12cf418df7f82f803091fa16f139937ae
Minju Park <mlnjv016@gmail.com>
2023-05-23 17:47:18 +0900

    BOJ 24479 solution

--- Algorithm Test/Baekjoon/24479. 알고리즘 수업 - 깊이 우선 탐색 1.swift
+++ Algorithm Test/Baekjoon/24479. 알고리즘 수업 - 깊이 우선 탐색 1.swift
@@ -11,7 +11,7 @@ func B24479() {
     let n = nmr[0], m = nmr[1], r = nmr[2]
     var visited = [Int](repeating: 0, count: n + 1)
     var graph = [[Int]](repeating: [], count: n + 1)
-    var depth = 1
+    var depth = 1 // 방문 노드(순서)
     for _ in 0..<m {
         let uv = readLine()!.split(separator: " ").map{Int($0)!}
         graph[uv[0]].append(uv[1])
@@ -21,8 +21,8 @@ func B24479() {
     dfs(r)
     for i in 1..<visited.count { print(visited[i]) }
 
-    func dfs(_ start: Int) {
-        visited[start] = depth // 시작 정점 start을 방문 했다고 표시
+    func dfs(_ start: Int) { // start는 방문 순서
+        visited[start] = depth // 시작 정점 start에 방문 순서 넣기
         for node in graph[start].sorted() { // 정점 번호를 오름차순으로 방문
             if visited[node] == 0 { depth += 1; dfs(node) }
         }
