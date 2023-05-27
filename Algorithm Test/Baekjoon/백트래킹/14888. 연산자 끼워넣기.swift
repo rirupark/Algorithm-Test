1a9adc0164c98c4e2dbe030680e20766a4ed3e27
Minju Park <mlnjv016@gmail.com>
2023-05-27 19:55:13 +0900

    BOJ 14888, 14889 solution

--- Algorithm Test/Baekjoon/백트래킹/14888. 연산자 끼워넣기.swift
+++ Algorithm Test/Baekjoon/백트래킹/14888. 연산자 끼워넣기.swift
@@ -4,5 +4,38 @@
 //
 //  Created by 박민주 on 2023/05/19.
 //
+// 브루트포스 알고리즘, 백트래킹
 
-import Foundation
+func B14888() {
+    let n = Int(readLine()!)!
+    let nums = readLine()!.split(separator: " ").map{Int($0)!}
+    var op = readLine()!.split(separator: " ").map{Int($0)!}
+    var maxValue = Int.min, minValue = Int.max
+
+    func operate(_ idx: Int, _ result: Int) {
+        if idx == n { // exit
+            maxValue = max(result, maxValue)
+            minValue = min(result, minValue)
+            return
+        }
+        for i in 0..<4 {
+            if op[i] < 1 { continue }
+            op[i] -= 1
+            switch i {
+            case 0:
+                operate(idx + 1, result + nums[idx])
+            case 1:
+                operate(idx + 1, result - nums[idx])
+            case 2:
+                operate(idx + 1, result * nums[idx])
+            case 3:
+                operate(idx + 1, result / nums[idx])
+            default:
+                break
+            }
+            op[i] += 1
+        }
+    }
+    operate(1, nums[0])
+    print("\(maxValue)\n\(minValue)")
+}
--- Algorithm Test/Baekjoon/백트래킹/14889. 스타트와 링크.swift
+++ Algorithm Test/Baekjoon/백트래킹/14889. 스타트와 링크.swift
@@ -4,5 +4,44 @@
 //
 //  Created by 박민주 on 2023/05/19.
 //
+// 브루트포스 알고리즘, 백트래킹
 
-import Foundation
+func B14889() {
+    let n = Int(readLine()!)!
+    var array = [[Int]]()
+    var startTeam = 0, linkTeam = 0
+    var visited = [Bool](repeating: false, count: n)
+    var depth = 1, minValue = Int.max
+    for _ in 0..<n {
+        array.append(readLine()!.split(separator: " ").map{Int($0)!})
+    }
+
+    func divideTeam(_ depth: Int, _ start: Int) {
+        if depth == n / 2 {
+            startTeam = 0; linkTeam = 0
+            for i in 0..<n {
+                for j in 0..<n {
+                    if visited[i] && visited[j] {
+                        startTeam += array[i][j]
+                    }
+                    if !visited[i] && !visited[j] {
+                        linkTeam += array[i][j]
+                    }
+                }
+            }
+            minValue = min(abs(startTeam - linkTeam), minValue)
+            return
+        }
+        for i in start..<n {
+            if !visited[i] {
+                visited[i] = true
+                divideTeam(depth + 1, i)
+                visited[i] = false
+            }
+        }
+    }
+
+    divideTeam(0, 0)
+    print(minValue)
+
+}
