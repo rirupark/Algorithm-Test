816bbc21a8efd7327450a6243bc4bcd2df684381
Minju Park <mlnjv016@gmail.com>
2023-05-30 22:13:10 +0900

    BOJ 28138

---
+++ Algorithm Test/Baekjoon/기타/28138. 재밌는 나머지 연산.swift
@@ -0,0 +1,19 @@
+//
+//  28138. 재밌는 나머지 연산.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/05/30.
+//
+// 수학, 정수론, 소수 판정
+
+func B28138() {
+    
+}
+
+//시간 초과
+//let nr = readLine()!.split(separator: " ").map{Int($0)!}
+//var result = 0
+//for num in 1...nr[0] {
+//    if nr[0] % num == nr[1] { result += num }
+//}
+//print(result)
