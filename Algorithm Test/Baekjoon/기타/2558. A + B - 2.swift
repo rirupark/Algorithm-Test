5fae22a350a1be60b43b3bab642c6ce056a5b055
Minju Park <mlnjv016@gmail.com>
2023-06-18 23:54:39 +0900

    BOJ 2558 solution

---
+++ Algorithm Test/Baekjoon/기타/2558. A + B - 2.swift
@@ -0,0 +1,11 @@
+//
+//  2558. A + B - 2.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/06/18.
+//
+
+func B2558() {
+    let a = Int(readLine()!)!, b = Int(readLine()!)!
+    print(a + b)
+}
