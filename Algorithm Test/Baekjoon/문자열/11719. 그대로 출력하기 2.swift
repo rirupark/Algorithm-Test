d81d9bf04100a2978fff6f3ccc0c2401bc60d988
Minju Park <mlnjv016@gmail.com>
2023-06-24 23:16:06 +0900

    BOJ 11719 solution

---
+++ Algorithm Test/Baekjoon/문자열/11719. 그대로 출력하기 2.swift
@@ -0,0 +1,13 @@
+//
+//  11719. 그대로 출력하기 2.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/06/24.
+//
+// 구현, 문자열
+
+func B11719() {
+    while let input = readLine() {
+        print(input)
+    }
+}
