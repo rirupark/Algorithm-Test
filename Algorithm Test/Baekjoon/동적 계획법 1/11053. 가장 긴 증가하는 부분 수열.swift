c0a3e8d6fa2e3bd536e650e5b6cfe8f3b92f7450
Minju Park <mlnjv016@gmail.com>
2023-07-06 20:42:27 +0900

    BOJ 11053 solution

---
+++ Algorithm Test/Baekjoon/동적 계획법 1/11053. 가장 긴 증가하는 부분 수열.swift
@@ -0,0 +1,19 @@
+//
+//  11053. 가장 긴 증가하는 부분 수열.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/07/06.
+//
+// DP
+
+func B11053() {
+    let n = Int(readLine()!)!
+    var a = readLine()!.split(separator: " ").map{Int($0)!}
+    var cache = [Int](repeating: 1, count: n)
+    for i in 1..<n {
+        for j in 0..<i {
+            if a[i] > a[j] { cache[i] = max(cache[i], cache[j] + 1) }
+        }
+    }
+    print(cache.max()!)
+}
