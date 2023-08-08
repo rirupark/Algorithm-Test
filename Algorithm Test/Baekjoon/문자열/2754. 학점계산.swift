80c1c9f30365ccda56af289565ef3f17d2115888
Minju Park <mlnjv016@gmail.com>
2023-08-08 23:20:32 +0900

    BOJ 2754 solution

---
+++ Algorithm Test/Baekjoon/문자열/2754. 학점계산.swift
@@ -0,0 +1,12 @@
+//
+//  2754. 학점계산.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/08/08.
+//
+
+func B2754() {
+    let grade = readLine()!
+    let dic = ["A+": 4.3, "A0": 4.0, "A-": 3.7, "B+": 3.3, "B0": 3.0, "B-": 2.7, "C+": 2.3, "C0": 2.0, "C-": 1.7, "D+": 1.3, "D0": 1.0, "D-": 0.7, "F": 0.0]
+    print(dic[grade]!)
+}
