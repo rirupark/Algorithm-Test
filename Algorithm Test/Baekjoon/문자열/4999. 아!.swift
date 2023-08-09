926ae10d3e1460a223a19b49e114478f88032804
Minju Park <mlnjv016@gmail.com>
2023-08-09 18:52:06 +0900

    BOJ 4999 solution

---
+++ Algorithm Test/Baekjoon/문자열/4999. 아!.swift
@@ -0,0 +1,11 @@
+//
+//  4999. 아!.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 2023/08/09.
+//
+
+func B4999() {
+    let jaehwan = readLine()!, doctor = readLine()!
+    print(jaehwan.count < doctor.count ? "no" : "go")
+}
