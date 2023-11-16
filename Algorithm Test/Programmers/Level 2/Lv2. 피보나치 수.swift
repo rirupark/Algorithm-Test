f15f14be32a47732e5cadf5d23100f573f0c378d
Minju Park <mlnjv016@gmail.com>
2023-11-16 23:37:42 +0900

    PG Lv2. 피보나치 수 solution

---
+++ Algorithm Test/Programmers/Level 2/Lv2. 피보나치 수.swift
@@ -0,0 +1,12 @@
+//
+//  Lv2. 피보나치 수.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 11/16/23.
+//
+
+//func solution(_ n:Int) -> Int {
+//    var cache = [0, 1]
+//    for i in 2...n { cache.append(cache[i - 2] % 1234567 + cache[i - 1] % 1234567) }
+//    return cache[n] % 1234567
+//}
