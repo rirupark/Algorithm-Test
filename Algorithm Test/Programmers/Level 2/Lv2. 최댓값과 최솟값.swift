ce491f1a24a2b3a21c7724764737fc13c1d31d5b
Minju Park <mlnjv016@gmail.com>
2023-11-16 23:37:22 +0900

    PG Lv2. 최댓값과 최솟값 solution

---
+++ Algorithm Test/Programmers/Level 2/Lv2. 최댓값과 최솟값.swift
@@ -0,0 +1,11 @@
+//
+//  Lv2. 최댓값과 최솟값.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 11/16/23.
+//
+
+//func solution(_ s:String) -> String {
+//    let numArr = s.split(separator: " ").map{Int($0)!}
+//    return String(numArr.min()!) + " " + String(numArr.max()!)
+//}
