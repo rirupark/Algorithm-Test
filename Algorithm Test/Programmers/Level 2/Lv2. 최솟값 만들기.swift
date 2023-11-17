4d4fe519524140f436cccd511cd8ba695379f325
Minju Park <mlnjv016@gmail.com>
2023-11-17 21:07:40 +0900

    PG Lv2. 최솟값 만들기 solution

---
+++ Algorithm Test/Programmers/Level 2/Lv2. 최솟값 만들기.swift
@@ -0,0 +1,17 @@
+//
+//  Lv2. 최솟값 만들기.swift
+//  Algorithm Test
+//
+//  Created by 박민주 on 11/17/23.
+//
+
+import Foundation
+
+//func solution(_ A:[Int], _ B:[Int]) -> Int {
+//    var ans = 0
+//    var A = A, B = B
+//    A.sort(by: <)
+//    B.sort(by: >)
+//    for i in 0..<A.count { ans += A[i] * B[i] }
+//    return ans
+//}
